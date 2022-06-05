e7e0c71f498f8d1b24980f8086e804dfb6d9dfc6
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-06-05 08:57:25 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,53 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+    inline static const int FREE_SPACE = 0;
+    inline static const int HOUSE = 1;
+
+public:
+    int minTotalDistance(vector<vector<int>>& matrix) {
+        vector<int> startRowsInAscendingOrder = findStartRowsInAscendingOrder(matrix);
+        vector<int> startColumnsInAscendingOrder = findStartColumnsInAscendingOrder(matrix);
+        return calculateDistance(startRowsInAscendingOrder) + calculateDistance(startColumnsInAscendingOrder);
+    }
+
+private:
+    vector<int> findStartRowsInAscendingOrder(const vector<vector<int>>& matrix) {
+        vector<int> startRowsInAscendingOrder;
+        for (int row = 0; row < matrix.size(); ++row) {
+            for (int column = 0; column < matrix[0].size(); ++column) {
+                if (matrix[row][column] == HOUSE) {
+                    startRowsInAscendingOrder.push_back(row);
+                }
+            }
+        }
+        return startRowsInAscendingOrder;
+    }
+
+    vector<int> findStartColumnsInAscendingOrder(const vector<vector<int>>& matrix) {
+        vector<int> startColumnsInAscendingOrder;
+        for (int column = 0; column < matrix[0].size(); ++column) {
+            for (int row = 0; row < matrix.size(); ++row) {
+                if (matrix[row][column] == HOUSE) {
+                    startColumnsInAscendingOrder.push_back(column);
+                }
+            }
+        }
+        return startColumnsInAscendingOrder;
+    }
+
+    int calculateDistance(const vector<int>& positions) {
+        int front = 0;
+        int end = positions.size() - 1;
+        int totalDistance = 0;
+        while (front < end) {
+            totalDistance += positions[end] - positions[front];
+            ++front;
+            --end;
+        }
+        return totalDistance;
+    }
+};
